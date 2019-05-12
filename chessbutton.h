42ed3d9bcfc6b4376c92ff9ba5da9c81b1624864
Samuel Simpson <simpson880114@gmail.com>
2019-05-12 19:52:47 +0800

    demo simple one

---
+++ chessbutton.h
@@ -0,0 +1,9 @@
+#ifndef CHESSBUTTON_H
+#define CHESSBUTTON_H
+
+#include <QGraphicsRectItem>
+#include <QGraphicsItemGroup>
+#include <QGraphicsSceneHoverEvent>
+#include <QGraphicsSceneMouseEvent>
+
+#endif // CHESSBUTTON_H
